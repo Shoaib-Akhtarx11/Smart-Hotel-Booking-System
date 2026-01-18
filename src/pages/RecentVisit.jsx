3809c92c44c52ae687606594bd1c1398cd9f180b
HarshBhorde29 <harshsomnath.bhorde@cognizant.com>
2026-01-18 08:15:06 +0530

    Merge with Animesh

---
+++ src/pages/RecentVisit.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../components/layout/NavBar";
+
+function RecentVisit() {
+  return (
+    <div>
+        <NavBar />
+    </div>
+  );
+}
+
+export default RecentVisit;
