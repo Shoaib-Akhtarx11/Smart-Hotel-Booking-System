75b90f06096bc6014eafe4a64c36ac95c5576b2f
HarshBhorde29 <harshsomnath.bhorde@cognizant.com>
2026-01-18 09:21:06 +0530

    Added sidebar and recently viewed layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"
 import HotelList from "./pages/HotelList";
+import Sidebar from "./components/layout/Sidebar";
+import NavBar from "./components/layout/NavBar";
+import RecentViewedBody from "./components/features/RecentVisit/RecentVisitBody";
+import RecentVisit from "./pages/RecentVisit";
 
 const App = () => {
   return (
     <div>
+      
       <Routes>
-        <Route path="/" element = {<Home />}/>
-        <Route path="/hotelList" element = {<HotelList />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotelList" element={<HotelList />} />
+        <Route path="/recentvisit" element={<RecentVisit />} />
       </Routes>
     </div>
   )
--- src/pages/RecentVisit.jsx
+++ src/pages/RecentVisit.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-import NavBar from "../components/layout/NavBar";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import NavBar from '../components/layout/NavBar';
+import Sidebar from '../components/layout/Sidebar';
+import RecentViewedBody from '../components/features/RecentVisit/RecentVisitBody';
 
-function RecentVisit() {
+const RecentVisit = () => {
   return (
-    <div>
-        <NavBar />
-    </div>
+    <>
+    <NavBar />
+    <Container fluid>
+        
+    
+      <Row>
+        {/* Left Sidebar Column - Hidden on small screens if you want responsive behavior */}
+        <Col md={3} lg={2} className="p-0 border-end min-vh-100 d-none d-md-block">
+          <Sidebar />
+        </Col>
+
+        {/* Main Content Column */}
+        <Col xs={12} md={9} lg={10} className="p-4">
+          <RecentViewedBody />
+        </Col>
+      </Row>
+    </Container>
+    </>
   );
-}
+};
 
 export default RecentVisit;
